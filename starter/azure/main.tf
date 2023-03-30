f7ab0d753d4b8d2c759529258b0ea773649138c7
Pedro Alves <palves1945@hotmail.com>
2023-03-30 12:39:43 -0300

    Starter code 2023-03-30

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -1,5 +1,5 @@
 data "azurerm_resource_group" "udacity" {
-  name     = "Regroup_4gKqrgD_cn"
+  name     = "Regroup_2g0ZOql9u2VfzAGKM3TLMH"
 }
 
 resource "azurerm_container_group" "udacity" {
@@ -7,7 +7,7 @@ resource "azurerm_container_group" "udacity" {
   location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
   ip_address_type     = "Public"
-  dns_name_label      = "udacity-tscotto-azure"
+  dns_name_label      = "udacity-alves-azure"
   os_type             = "Linux"
 
   container {
@@ -16,8 +16,8 @@ resource "azurerm_container_group" "udacity" {
     cpu    = "0.5"
     memory = "1.5"
     environment_variables = {
-      "AWS_S3_BUCKET"       = "udacity-tscotto-aws-s3-bucket",
-      "AWS_DYNAMO_INSTANCE" = "udacity-tscotto-aws-dynamodb"
+      "AWS_S3_BUCKET"       = "uda-alves-aws-s3",
+      "AWS_DYNAMO_INSTANCE" = "uda-alves-dynamodb"
     }
     ports {
       port     = 3000
@@ -30,3 +30,66 @@ resource "azurerm_container_group" "udacity" {
 }
 
 ####### Your Additions Will Start Here ######
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
+#
+
+resource "azurerm_storage_account" "udacity" {
+  name                     = "udaalvessto"
+  resource_group_name      = data.azurerm_resource_group.udacity.name
+  location                 = data.azurerm_resource_group.udacity.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/sql_server
+
+resource "azurerm_mssql_server" "udacity" {
+  name                         = "uda-alves-az-mssql"
+  resource_group_name          = data.azurerm_resource_group.example.name
+  location                     = data.azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "justadminuda"
+  administrator_login_password = "minE#mikA#1945@"
+
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_database
+
+resource "azurerm_mssql_database" "udacity" {
+  name           = "uda-mssql-db"
+  server_id      = azurerm_mssql_server.udacity.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 150
+  read_scale     = false
+  sku_name       = "S0"
+  zone_redundant = false
+
+  tags = {
+    name        = "uda-mssql-db"
+    environment = "udacity"
+  }
+}
+
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan
+
+resource "azurerm_service_plan" "udacity" {
+  name                = "uda-sp"
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  location            = data.azurerm_resource_group.udacity.location
+  os_type             = "Windows"
+  sku_name            = "P1v2"
+}
+
+
+resource "azurerm_windows_web_app" "udacity" {
+  name                = "udaalvesazdotnet"
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  location            = data.azurerm_service_plan.udacity.location
+  service_plan_id     = azurerm_service_plan.udacity.id
+
+  site_config {}
+}
+
+####
+
